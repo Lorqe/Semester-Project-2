6472b7e451e3ef61b59729f723cc79232eeb8e62
Lorqe <henrik@insitemedia.no>
2019-11-20 07:36:43 +0100

    Player instructions added

--- js/characters.js
+++ js/characters.js
@@ -32,16 +32,20 @@ request.onload = function() {
                 selectBtn.classList.add("selectBtn");
                 selectBtn.value = data[i].name;
                 var p = 1;
+                var pHolder = document.getElementById("playerNum");
+                var instructions = document.getElementById("playerInstruction");
                 selectBtn.onclick = function() {
                     
                     if(p === 1) {
                         this.innerHTML = "selected";
                         console.log("Player " + p + " has selected: " + this.value);
                         p++;
+                        pHolder.innerHTML = p;
                     } else if(p === 2) {
                         this.innerHTML = "selected";
                         console.log("Player " + p + " has selected: " + this.value);
                         p++;
+                        instructions.innerHTML = "You Can Now Start The Game";
                     } else if (p > 2) {
 
                     }  
